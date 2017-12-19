f0273d6afcb764ff39faa2993fd839d51176a94c
mdatsev <martin.datsev.p@gmail.com>
2017-12-19 20:20:44 +0200

    added RSA class

---
+++ lib/RSA.rb
@@ -0,0 +1,67 @@
+require "prime"
+
+def twoprimes
+    Prime.first(100).drop(60).shuffle[0..1]
+end
+
+def invmod(a, m)
+  i = 0
+  loop do
+    if a * i % m == 1
+      return i
+    end
+    i+=1
+  end
+end
+
+class RSA
+    def initialize n, e, d
+        @n = n
+        @e = e
+        @d = d
+      #initializes this instance of RSA with a n,e and d variables of type int. 'n','e' are the public key and 'd' is the private one. This n,e,d should be used when encrypting and decrypting the message
+    end
+    
+    def n
+      @n
+    end
+    
+    def e
+      @e
+    end
+    
+    def d
+      @d
+    end
+    
+    def new_key
+        p, q = twoprimes
+        n = p*q
+        ctfn = (p-1).lcm(q-1)
+        e = e
+        loop do
+            e = rand(1...ctfn)
+            break if e.gcd(ctfn) == 1
+        end
+        d = invmod(e, ctfn)
+        [n, e, d]
+       #generates a new 'n','e' and 'd' values following the RSA algorithm. Returns a new array of three elements where the first element is 'n', the second is 'e' and the third is 'd'. Each time it is called a new key must be returned.
+    end
+    
+    def encrypt message
+      message.chars.map { |ch| (ch.ord ** @e % @n).to_s(36) }.join('-')
+      #encrypts the message passed. The message is of type string. Encrypts each symbol of this string by using its ASCII number representation and returns the encrypted message.
+    end
+    
+    def decrypt message
+      message.split("-").map { |seg| (seg.to_i(36) ** @d % @n).chr }.join('')
+      #decrypts the message passed. The message is of type string. Decrypts each symbol of this string Encrypts each symbol of this string by using its ASCII number representationand returns the decrypted message. 
+    end
+end
+
+n = RSA.new 1,2,3
+p n.new_key;p n.new_key;p n.new_key;p n.new_key;
+p n.new_key;p n.new_key;p n.new_key;p n.new_key;
+p n.new_key;p n.new_key;p n.new_key;p n.new_key;
+p n.new_key;p n.new_key;p n.new_key;p n.new_key;
+p n.new_key;p n.new_key;p n.new_key;p n.new_key;
---
+++ spec/RSA_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/RSA'
+
+RSpec.describe RSA do
+    it "should preserve the message encrypt->decrypt with keys 3233, 17, 413" do
+        rsa = RSA.new(3233, 17, 413)
+        expect(rsa.decrypt(rsa.encrypt("henlo"))).to eq "henlo"
+    end
+    it "encrypted message should not be the same" do
+        rsa = RSA.new(*RSA.new(1,2,3).new_key)
+        expect(rsa.encrypt("henlo")).not_to eq "henlo"
+    end
+    it "should preserve the message enctypt->decrypt with generated key" do
+        rsa = RSA.new(*RSA.new(1,2,3).new_key)
+        expect(rsa.decrypt(rsa.encrypt("henlo"))).to eq "henlo"
+    end
+    it "should return the right n" do
+        rsa = RSA.new(123,0,0)
+        expect(rsa.n).to eq 123
+    end
+    it "should return the right e" do
+        rsa = RSA.new(0,321,0)
+        expect(rsa.e).to eq 321
+    end
+    it "should return the right d" do
+        rsa = RSA.new(0,0,567)
+        expect(rsa.d).to eq 567
+    end
+    it "should encrypt this basic test" do
+        rsa = RSA.new(627,241,49)
+        expect(rsa.encrypt("a")).to eq "ay"
+    end
+end
